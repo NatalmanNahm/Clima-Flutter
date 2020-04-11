108518a83aae3c9260da11376a5871eae6a1f267
Natalman <natalnahm@gmail.com>
2020-04-10 20:59:28 -0500

    Added an exception handler to the getLocation method.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -8,14 +8,17 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-
-
   void getLocation() async{
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
-  }
+    try{
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (e){
+      print(e);
+    }
 
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold();
