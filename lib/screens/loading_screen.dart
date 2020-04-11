2276d75a87902871b4d3e94b91fd5209b2aee004
Natalman <natalnahm@gmail.com>
2020-04-11 13:28:19 -0500

    Refactored the loadingScreen.dart to look a lot simpler and easy to read.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:clima/services/networking.dart';
+
+const kApiKey = 'dbcb16a1f918b30d4e0fb668f1487fbb';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,44 +11,31 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  void getLocation() async{
+  double lat;
+  double long;
+
+  void getLocationData() async{
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    lat = location.latitude;
+    long = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5'
+        '/weather?lat=$lat&lon=$long&appid=$kApiKey');
+
+    var weatherData = await networkHelper.getData();
+
+  }
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 
-  void getData() async{
-    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5'
-        '/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
 
-    if(response.statusCode == 200){
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      int condition = decodedData ['weather'] [0] ['id'];
-      double temp = decodedData['main'] ['temp'];
-      String city = decodedData['name'];
-
-      print(city);
-      print(condition);
-      print(temp);
-
-    } else{
-      print(response.statusCode);
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getLocation();
-  }
 }
