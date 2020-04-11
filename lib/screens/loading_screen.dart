f43cca7111d4a23a1ac7143fe1deba73808e22b7
Natalman <natalnahm@gmail.com>
2020-04-11 12:44:24 -0500

    using the json file to pull data needed with the help of covert package.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -25,7 +26,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getData() async{
     http.Response response = await http.get('https://samples.openweathermap.org/data/2.5'
         '/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
-    print(response.body);
+
+    if(response.statusCode == 200){
+      String data = response.body;
+
+      var condition = jsonDecode(data) ['weather'] [0] ['id'];
+      print(condition);
+
+      var temp = jsonDecode(data) ['main'] ['temp'];
+      print(temp);
+
+      var city = jsonDecode(data) ['name'];
+      print(city);
+
+    } else{
+      print(response.statusCode);
+    }
   }
 
   @override
